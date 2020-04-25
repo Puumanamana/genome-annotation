518f4fcf0544e68838802502336d283d162c306b
Cedric Arisdakessian <carisdak@hawaii.edu>
2020-04-25 10:11:37 -1000

    first commit

---
+++ genome_annotation/src/main.rs
@@ -0,0 +1,92 @@
+extern crate csv;
+extern crate clap;
+
+use std::io;
+use std::io::{BufWriter, Write};
+use std::io::prelude::*;
+
+use std::fs::File;
+
+use std::cmp::Reverse;
+use std::cmp::Ordering;
+
+use csv::{ReaderBuilder};
+use clap::{Arg, App};
+type Record = (String, String, u64, u64);
+
+// Database available at ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid
+fn parse_args() -> clap::ArgMatches<'static>{
+	
+	App::new("genome_annotation")
+		.version("0.0")
+		.about("Search for NCBI ID in taxdmp file")
+		.author("Cedric Arisdakessian")
+		.arg(Arg::with_name("db")
+			 .long("db")
+			 .takes_value(true)
+			 .help("path to taxdmp"))
+		.arg(Arg::with_name("query")
+			 .long("query")
+			 .takes_value(true)
+			 .help("path to ncbi ids file"))
+		.arg(Arg::with_name("output")
+			 .short("o")
+			 .help("output file path")
+			 .default_value("output.csv"))
+		.get_matches()
+}
+
+fn main() -> io::Result<()> {
+
+	let args = parse_args();
+	let db_path = args.value_of("db").unwrap();
+	let query_path = args.value_of("query").unwrap();
+	let out_path = args.value_of("output").unwrap();
+
+	let mut db_rdr = ReaderBuilder::new()
+        .delimiter(b'\t')
+		.has_headers(true)
+        .from_path(db_path)
+		.expect("Could not find database");
+
+	let mut query_handle = File::open(query_path)?;
+
+	let output_wtr = File::create(out_path).expect("Unable to create file");
+	let mut output_wtr = BufWriter::new(output_wtr);
+
+	let mut ids = String::new();
+	query_handle.read_to_string(&mut ids).expect("Unable to read data");
+
+	let mut ids: Vec<&str> = ids.trim_end().split('\n').collect();
+	(&mut ids).sort_by_key(|&x| Reverse(x));
+
+	let mut cur_query = (&mut ids).pop().unwrap();
+
+	for entry in db_rdr.deserialize() {
+		let (acc, _acc_full, taxid, gid): Record = entry?;
+		// println!("{} vs {}", cur_query, acc);
+
+		let comparison = &cur_query.cmp(&acc);
+		
+		if *comparison == Ordering::Greater {
+			continue
+		}
+
+		if *comparison == Ordering::Equal {
+			// println!("{},{},{}", acc, taxid, gid);
+			let info = format!("{},{},{}\n", acc, taxid, gid);
+			output_wtr.write_all(info.as_bytes()).expect("Unable to write data");
+		}
+
+		if *comparison == Ordering::Less {
+			eprintln!("{} not found", cur_query);
+		}
+
+		cur_query = match (&mut ids).pop() {
+			Some(x) => x,
+			None => break
+		};		
+	}
+
+	Ok(())
+}
---
+++ taxid2taxonomy.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import subprocess as sp
+import pandas as pd
+from ete3 import NCBITaxa
+
+import argparse
+
+SRC_DIR = Path(__file__).parent
+RUST_APP = '{}/genome_annotation/target/release/genome_annotation'.format(SRC_DIR)
+
+def parse_args():
+    '''
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--taxids', type=str, default="taxids.csv")
+    parser.add_argument('--accessions', type=str, default=None)
+    parser.add_argument('--ranks', type=str, nargs='*')
+    args = parser.parse_args()
+
+    if args.accessions is not None:
+        args.accessions = Path(args.accessions).resolve()
+    if args.taxids is not None:
+        args.taxids = Path(args.taxids).resolve()
+
+    return args
+
+def get_taxids(accessions_file, output):
+    cmd = [RUST_APP,
+           '--query', str(accessions_file),
+           '--db', '{}/db/nucl_gb.accession2taxid'.format(SRC_DIR),
+           '-o', str(output)]
+    sp.run(cmd, check=True)
+
+def taxid2lineage(db, acc, taxid, ranks=None):
+    try:
+        lineage = db.get_lineage(taxid)
+    except ValueError:
+        print('No match for {} ({})'.format(acc, taxid))
+        return
+    names = pd.Series(db.get_taxid_translator(lineage)).rename(acc)
+    names.index = pd.Series(db.get_rank(names.index)).values
+    names.drop('no rank', inplace=True)
+
+    if ranks:
+        names = names.reindex(ranks)
+
+    return names
+
+def process_all_taxids(query_file, ranks=None):
+    data = pd.read_csv(query_file, dtype=str, header=None).dropna()
+    ncbi = NCBITaxa()
+
+    lineages = []
+
+    for i, (acc, taxid, _) in enumerate(data.to_numpy()):
+        print("{:.1%}".format(i/len(data)), end='\r')
+
+        names = taxid2lineage(ncbi, acc, taxid, ranks=ranks)
+
+        if names is not None:
+            lineages.append(names)
+
+    lineage_df = pd.concat(lineages, axis=1).T
+    lineage_df.dropna(how='all').drop_duplicates().to_csv('lineages.csv')
+
+def main():
+    '''
+    '''
+
+    args = parse_args()
+
+    if args.accessions:
+        get_taxids(args.accessions, output=args.taxids)
+
+    process_all_taxids(args.taxids, ranks=args.ranks)
+            
+
+if __name__ == '__main__':
+    main()
